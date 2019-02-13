a44ad5ad463a580b8ca87fc28edeffe240b9bfa1
Greg Khmara <gkhmara@gmail.com>
2019-02-12 20:30:13 -0800

    initial commit

---
+++ includes/footer.inc.html.php
@@ -0,0 +1,28 @@
+<footer>
+    <div class="grid-container">
+        <div class="item1">
+            <div class="footer_menu">
+                <ul>
+                    <li><a href="#">FAQ</a></li>
+                    <li><a href="#">Registration</a></li>
+                    <li><a href="#">Course Details</a></li>
+                    <li><a href="#">Contact Page</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="item2">
+
+            <a href="http://twitter.com"><img src="img/facebook.png" alt="Twitter Logo" class="social-icon"></a>
+
+        </div>
+        <div class="item3">
+            <a href="http://facebook.com"><img src="img/twitter.png" alt="Facebook Logo" class="social-icon"></a>
+        </div>
+        <div class="item4">
+            <a href="#"><img src="img/weather.png" alt="Facebook Logo" class="social-icon"></a>
+        </div>
+
+    </div>
+
+    <div>&copy; 2019 Greg Khmara.</div>
+</footer>
---
+++ includes/main.inc.html.php
@@ -0,0 +1,53 @@
+<main>
+
+    <div id="hero">
+        <img src="img/blank900x300.jpg" alt="Downtown Portland">
+    </div>
+
+
+    <!--
+        <div class="slideshow-container">
+            <div class="mySlides fade">
+                <img src="img/tempImage.jpg" style="width:100%">
+                <div class="text">Image Caption</div>
+            </div>
+        </div>
+        <br>
+-->
+
+
+    <section>
+
+        <h2>ABOUT ACE IN THE HOLE MULTISPORT EVENTS</h2>
+
+        <p>Ace in the Hole Multisport Events is proud to offer running and triathlon events to athletes of all shapes and sizes, national origins, sexual orientations and cultural backgrounds. We offer Events for Every Body.</p>
+
+    </section>
+
+    <section>
+
+        <h2>ABOUT THE EVENT</h2>
+        <p>The Annual Ace in the Hole MultiSport Weekend is a legendary event in the Oregon triathlon and running community. It has become a traditional destination race for athletes from across the nation.</p>
+        <p>There is something for every level of athletic ability. The weekend includes a first timer triathlon, a sprint, Olympic, and Half-Iron triathlons and 10K and Half marathon runs. Come to experience your first race or come to compete to win, but make sure you come to have fun!</p>
+    </section>
+
+    <section class="section group">
+
+        <div class="col span_1_of_3 span_1_of_2">
+            <h3>Important Info</h3>
+            <p>Nam vel lectus et libero venenatis efficitur suscipit sed lacus. Maecenas sed purus in turpis semper pretium sollicitudin ac ipsum. Sed id libero viverra, pharetra felis eu, malesuada elit.</p>
+        </div>
+
+        <div class="col span_1_of_3 span_1_of_2">
+            <h3>Important Info</h3>
+            <p>Aliquam ultricies egestas tempus. Praesent ultricies lobortis massa in fermentum. Donec maximus scelerisque viverra. Quisque mattis consequat nulla, ac tempor nibh blandit sed.</p>
+        </div>
+
+        <div class="col span_1_of_3 span_1_of_2">
+            <h3>Important Info</h3>
+            <p>Duis ac purus et massa ultricies molestie. Nulla at interdum lacus. Suspendisse potenti. Suspendisse gravida orci quis urna placerat rutrum.</p>
+        </div>
+
+    </section>
+
+</main>
