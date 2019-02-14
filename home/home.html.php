fe8aa2f3f98dd704952c5d637680c0c2d0cccb74
gkhmara <34079752+gkhmara@users.noreply.github.com>
2019-02-13 18:58:15 -0800

    Add files via upload

--- home/home.html.php
+++ home/home.html.php
@@ -7,9 +7,6 @@
     <link rel="stylesheet" href="css/normalize.css">
     <link href='http://fonts.googleapis.com/css?family=Changa+One|Open+Sans:400italic,700italic,400,700,800' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="css/main.css">
-    <link rel="stylesheet" href="css/responsive.css">
-    <link rel="stylesheet" href="css/navigation.css">
-    <link rel="stylesheet" href="css/grid.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
@@ -17,7 +14,6 @@
     <?php include 'includes/header.inc.html.php'; ?>
     <?php include 'includes/main.inc.html.php'; ?>
     <?php include 'includes/footer.inc.html.php'; ?>
-
     <script src="js/slideshow.js"></script>
 </body>
 
