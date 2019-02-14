3726dc8ca78303338318219b8e71d521e351bea4
gkhmara <34079752+gkhmara@users.noreply.github.com>
2019-02-13 18:56:47 -0800

    Add files via upload

--- includes/header.inc.html.php
+++ includes/header.inc.html.php
@@ -1,10 +1,10 @@
-<header class="header">
+<header>
     <input class="menu-btn" type="checkbox" id="menu-btn" />
     <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
-    <h1 id="logo">ACE IN THE HOLE</h1>
+    <h1 id="logo">ACE IN THE HOLE MULTISPORT EVENTS</h1>
     <ul class="menu">
-        <li><a href="#">FAQ</a></li>
         <li><a href="#">Registration</a></li>
+        <li><a href="#">FAQ</a></li>
         <li><a href="#">Course Details</a></li>
         <li><a href="#">Contact Page</a></li>
     </ul>
--- includes/main.inc.html.php
+++ includes/main.inc.html.php
@@ -1,53 +1,28 @@
 <main>
-
-    <div id="hero">
-        <img src="img/blank900x300.jpg" alt="Downtown Portland">
-    </div>
-
-
-    <!--
-        <div class="slideshow-container">
-            <div class="mySlides fade">
-                <img src="img/tempImage.jpg" style="width:100%">
-                <div class="text">Image Caption</div>
-            </div>
-        </div>
-        <br>
--->
-
-
-    <section>
-
-        <h2>ABOUT ACE IN THE HOLE MULTISPORT EVENTS</h2>
-
-        <p>Ace in the Hole Multisport Events is proud to offer running and triathlon events to athletes of all shapes and sizes, national origins, sexual orientations and cultural backgrounds. We offer Events for Every Body.</p>
-
+    <section id="hero">
+        <img src="img/blank900x300.jpg">
     </section>
-
     <section>
-
-        <h2>ABOUT THE EVENT</h2>
-        <p>The Annual Ace in the Hole MultiSport Weekend is a legendary event in the Oregon triathlon and running community. It has become a traditional destination race for athletes from across the nation.</p>
-        <p>There is something for every level of athletic ability. The weekend includes a first timer triathlon, a sprint, Olympic, and Half-Iron triathlons and 10K and Half marathon runs. Come to experience your first race or come to compete to win, but make sure you come to have fun!</p>
+        <h2>LOREM IPSUM DOLOR SIT AMET CONSECTETUR</h2>
+        <p>Ut pretium commodo ipsum vel efficitur. Duis finibus velit a ante varius, non ultrices tellus interdum. Curabitur condimentum sed erat sed tristique. Pellentesque pharetra sapien a pellentesque mattis.</p>
     </section>
-
+    <section>
+        <h2>QUISQUE EST METUS</h2>
+        <p>Maecenas vitae enim ac eros ultricies malesuada eu sed urna. Fusce consequat, felis non sagittis pretium, tellus velit pellentesque lectus, at bibendum eros turpis a justo. Fusce lacinia leo non dapibus fringilla.</p>
+        <p>Integer egestas risus ac blandit pharetra. Praesent mi purus, convallis dictum nunc nec, sagittis commodo est. Quisque vulputate magna sed blandit interdum. Nunc non tristique neque. Suspendisse potenti. Ut nec rutrum ipsum. Morbi erat lectus, faucibus vel sem id, lobortis tempor sapien. Sed ultricies tellus nec libero ullamcorper ornare eu quis augue.</p>
+    </section>
     <section class="section group">
-
         <div class="col span_1_of_3 span_1_of_2">
-            <h3>Important Info</h3>
+            <h3>Curabitur tincidunt.</h3>
             <p>Nam vel lectus et libero venenatis efficitur suscipit sed lacus. Maecenas sed purus in turpis semper pretium sollicitudin ac ipsum. Sed id libero viverra, pharetra felis eu, malesuada elit.</p>
         </div>
-
         <div class="col span_1_of_3 span_1_of_2">
-            <h3>Important Info</h3>
+            <h3>Praesent mi purus</h3>
             <p>Aliquam ultricies egestas tempus. Praesent ultricies lobortis massa in fermentum. Donec maximus scelerisque viverra. Quisque mattis consequat nulla, ac tempor nibh blandit sed.</p>
         </div>
-
         <div class="col span_1_of_3 span_1_of_2">
-            <h3>Important Info</h3>
+            <h3>Sed ultricies tellus</h3>
             <p>Duis ac purus et massa ultricies molestie. Nulla at interdum lacus. Suspendisse potenti. Suspendisse gravida orci quis urna placerat rutrum.</p>
         </div>
-
     </section>
-
 </main>
